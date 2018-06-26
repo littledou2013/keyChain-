df7ebf5da026898bcca673450afabc3fb6b4b6d5
陈小双 <chenxiaoshuang@qingting.fm>
2018-06-26 16:21:44 +0800

    KeyChain保存密码小示例

--- KeyChain保存密码/ViewController.m
+++ KeyChain保存密码/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
-
+#import "SSKeychain.h"
+#define LDLoginServiceName @"LDLoginServiceName"
+#define LDLoginUserName @"LDLoginUserName"
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwardTextField;
 
 @end
 
@@ -16,13 +20,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self loadUserInfo];
+}
+
+- (IBAction)login:(id)sender {
+    if ([self.userNameTextField.text length] <= 0 || [self.passwardTextField.text length] <= 0) {
+        return;
+    }
+    //登录网络请求省略
+    //登录成功保存账号密码
+    [self savePwdWithAccunt:self.userNameTextField.text Pwd:self.passwardTextField.text];
+}
+
+//加载本地用户信息
+-(void)loadUserInfo{
+    //加载账号
+    self.userNameTextField.text = [[NSUserDefaults standardUserDefaults] stringForKey:LDLoginUserName];
+    //找出所有的账号
+    self.passwardTextField.text = [SSKeychain passwordForService:LDLoginServiceName account:self.userNameTextField.text];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+//保存账号密码
+-(void)savePwdWithAccunt:(NSString *)accunt Pwd:(NSString *)pwd{
+    //保存账号
+    [[NSUserDefaults standardUserDefaults] setObject:accunt forKey:LDLoginUserName];
+    //同步 -- 立刻马上保存!
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    //保存密码
+    /**
+     *  参数
+     *  1. 密码明文
+     *  2. 服务,可以随便写,但是他是APP的一个标识,建议用BundleID
+     *  3. 账号,用户名
+     */
+    [SSKeychain setPassword:pwd forService:LDLoginServiceName account:accunt];
 }
 
 
