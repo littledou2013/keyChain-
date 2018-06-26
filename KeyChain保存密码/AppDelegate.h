27074698ebc916996cca685f50806065b087ca4e
陈小双 <chenxiaoshuang@qingting.fm>
2018-06-26 16:08:28 +0800

    Initial Commit

---
+++ KeyChain保存密码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyChain保存密码
+//
+//  Created by cxs on 2018/6/26.
+//  Copyright © 2018年 cxs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
